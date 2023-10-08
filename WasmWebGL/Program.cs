9d09395254a849ff0869aef8a7125d8eb2eaa2b3
Marcin Nowak <malcinn@gmail.com>
2023-10-08 13:16:09 +0200

    Initial commit

---
+++ WasmWebGL/Program.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Tasks;
+using static WasmWebGL.WebGL;
+
+await JSHost.ImportAsync("webgl-bindings.js", "./webgl-bindings.js");
+
+var canvasWidth = 720;
+var canvasHeight = 720;
+
+InitCanvasWebGL(canvasWidth, canvasHeight);
+
+glViewport(0, 0, canvasWidth, canvasHeight);
+glClearColor(0, 0, 0, 1);
+glEnable(GL_DEPTH_TEST);
+
+//var vao = glGenVertexArrays();
+//glBindVertexArray(vao);
+
+float[] vertices = {
+    // https://en.wikipedia.org/wiki/Tetrahedron
+     1.0f,  0.0f, -1 / MathF.Sqrt(2), 1f, 0f, 0f,
+    -1.0f,  0.0f, -1 / MathF.Sqrt(2), 0f, 1f, 0f,
+     0.0f,  1.0f,  1 / MathF.Sqrt(2), 0f, 0f, 1f,
+     0.0f, -1.0f,  1 / MathF.Sqrt(2), 1f, 1f, 1f
+};
+
+var vbo = glGenBuffers();
+
+glBindBuffer(GL_ARRAY_BUFFER, vbo);
+glBufferData(
+    GL_ARRAY_BUFFER,
+    vertices.Length * sizeof(float),
+    vertices,
+    GL_STATIC_DRAW);
+
+glVertexAttribPointer(0, 3, GL_FLOAT, 0, 6 * sizeof(float), 0);
+glEnableVertexAttribArray(0);
+glVertexAttribPointer(1, 3, GL_FLOAT, 0, 6 * sizeof(float), 3 * sizeof(float));
+glEnableVertexAttribArray(1);
+
+var ebo = glGenBuffers();
+byte[] indices =
+{
+    0, 1, 2,
+    0, 1, 3,
+    0, 2, 3,
+    1, 2, 3
+};
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+glBufferData(
+    GL_ELEMENT_ARRAY_BUFFER,
+    indices.Length * sizeof(byte),
+    indices,
+    GL_STATIC_DRAW);
+
+var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+glShaderSource(vertexShader, """
+    #version 300 es
+
+    in vec3 vertex_position;
+    in vec3 vertex_color_in;
+    out vec3 vertex_color;
+
+    uniform mat4 perspective;
+    uniform mat4 model;
+
+    void main() {
+        gl_Position = perspective * model * vec4(vertex_position, 1.0);
+        vertex_color = vertex_color_in;
+    }
+    """);
+glCompileShader(vertexShader);
+
+if (!glGetShaderParameter(vertexShader, GL_COMPILE_STATUS))
+{
+    Console.WriteLine("Failed to compile vertex shader");
+    Console.WriteLine(glGetShaderInfoLog(vertexShader));
+}
+
+var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+glShaderSource(fragmentShader, """
+    #version 300 es
+    precision highp float;
+
+    in vec3 vertex_color;
+    out vec4 fragColor;
+
+    void main()
+    {
+        fragColor = vec4(vertex_color, 1.0);
+    }
+    """);
+glCompileShader(fragmentShader);
+
+if (!glGetShaderParameter(fragmentShader, GL_COMPILE_STATUS))
+{
+    Console.WriteLine("Failed to compile fragment shader");
+    Console.WriteLine(glGetShaderInfoLog(fragmentShader));
+}
+
+var shaderProgram = glCreateProgram();
+glAttachShader(shaderProgram, vertexShader);
+glAttachShader(shaderProgram, fragmentShader);
+glLinkProgram(shaderProgram);
+glUseProgram(shaderProgram);
+
+var perspective = Matrix4x4.CreatePerspectiveFieldOfView(
+    fieldOfView: AngleToRadians(45f),
+    aspectRatio: (float)canvasWidth / canvasHeight,
+    nearPlaneDistance: 0.1f,
+    farPlaneDistance: 500f);
+
+glUniformMatrix4fv(
+    glGetUniformLocation(shaderProgram, "perspective"),
+    perspective);
+
+var time = DateTime.UtcNow;
+
+while (true)
+{
+    var result = Matrix4x4.CreateRotationY((float)DateTime.UtcNow.Subtract(time).TotalSeconds)
+        * Matrix4x4.CreateRotationX((float)DateTime.UtcNow.Subtract(time).TotalSeconds / 2.0f)
+        * Matrix4x4.CreateTranslation(0, 0, -5f);
+
+    glUniformMatrix4fv(
+        glGetUniformLocation(shaderProgram, "model"),
+        result);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glDrawElements(GL_TRIANGLES, indices.Length, GL_UNSIGNED_BYTE, 0);
+
+    await Task.Delay(1);
+}
+
+static float AngleToRadians(float degrees) => degrees * (MathF.PI / 180f);
---
+++ WasmWebGL/WebGL.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
+namespace WasmWebGL;
+
+public static partial class WebGL
+{
+    public static void glBufferData(int target, int size, List<float> data, int usage)
+    {
+        glBufferData(target, size, CollectionsMarshal.AsSpan(data), usage);
+    }
+
+    public static void glBufferData(int target, int size, byte[] data, int usage)
+    {
+        glBufferData(target, size, data.AsSpan(), usage);
+    }
+
+    public static void glBufferData(int target, int size, Span<byte> data, int usage)
+    {
+        glBufferData(target, size, ToJson(data), usage, GL_UNSIGNED_BYTE);
+    }
+
+    public static void glBufferData(int target, int size, float[] data, int usage)
+    {
+        glBufferData(target, size, data.AsSpan(), usage);
+    }
+
+    public static void glBufferData(int target, int size, Span<float> data, int usage)
+    {
+        glBufferData(target, size, ToJson(data), usage, GL_FLOAT);
+    }
+
+    public static void glUniformMatrix4fv(int uniformLocation, Matrix4x4 data)
+    {
+        unsafe
+        {
+            void* ptr = &data;
+            glUniformMatrix4fv(uniformLocation, new Span<float>(ptr, 16), false);
+        }
+    }
+
+    public static void glUniformMatrix4fv(int uniformLocation, float[] data, bool transpose)
+    {
+        glUniformMatrix4fv(uniformLocation, data.AsSpan(), transpose);
+    }
+
+    public static void glUniformMatrix4fv(int uniformLocation, Span<float> data, bool transpose)
+    {
+        glUniformMatrix4fv(uniformLocation, ToJson(data), transpose);
+    }
+
+    private static string ToJson<T>(Span<T> data)
+    {
+        var stringBuilder = new StringBuilder();
+
+        stringBuilder.Append("[");
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            stringBuilder.Append(data[i].ToString());
+
+            if (i < data.Length - 1)
+            {
+                stringBuilder.Append(", ");
+            }
+        }
+
+        stringBuilder.Append("]");
+
+        return stringBuilder.ToString();
+    }
+}
